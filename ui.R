5033123f4f6b6d1863f4c560ab3f10f29b046fd6
Emanuel Carlsson <106364327+EmCarlss@users.noreply.github.com>
2024-02-11 08:24:33 +0100

    Version 1.3
    Added info about the source.

--- ui.R
+++ ui.R
@@ -119,7 +119,13 @@ ui <- fluidPage(theme = bs_theme(bootswatch = "minty"),
                          )
                 ),
                 tabPanel("Help",
-                         h5("Navigate seamlessly in the universe of european price statistics"),br(),p("This app enables to produce graphs for the all-items level or any subcomponent(s) in the european HICP (Harmonized Index of Consumer Prices). Currently graphs for indices, weights and annual rates can be produced.",br(),p(" "),"For the Index tab, users can set a new reference period (month or year), common for all selected index series.",br(),p(" "),"For the M/M-12 tab (annual rates of change), Ribe contributions are calculated for the level directly below the selected aggregate (following formula in Eurostat's HICP Methodological manual, p 182-183). The Ribe contribution is also described in"), HTML('<a href="https://www.oecd.org/sdd/prices-ppp/OECD-calculation-contributions-annual-inflation.pdf"), target="_blank">https://www.oecd.org/sdd/prices-ppp/OECD-calculation-contributions-annual-inflation.pdf</a>')
+                         h5("Navigate seamlessly in the universe of european price statistics"),
+                         br(),p("This app enables to produce graphs for the all-items level or any subcomponent(s) in the european HICP (Harmonized Index of Consumer Prices). Currently graphs for indices, weights and annual rates can be produced.",
+                                br(),p(" "),"For the Index tab, users can set a new reference period (month or year), common for all selected index series.",br(),p(" "),
+                                "For the M/M-12 tab (annual rates of change), Ribe contributions are calculated for the level directly below the selected aggregate (following formula in Eurostat's HICP Methodological manual, p 182-183). The Ribe contribution is also described in"),
+                                HTML('<a href="https://www.oecd.org/sdd/prices-ppp/OECD-calculation-contributions-annual-inflation.pdf"), target="_blank">https://www.oecd.org/sdd/prices-ppp/OECD-calculation-contributions-annual-inflation.pdf</a>'),
+                         br(),p(" "), "Data is retrieved using the 'eurostat' package from the Eurostat HICP database. Additional calculations are performed where needed." 
+                         
                          )
                 
         )
