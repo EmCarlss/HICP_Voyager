d06b97751003918f69df3417f2d4018500b06517
Emanuel Carlsson <106364327+EmCarlss@users.noreply.github.com>
2024-04-14 09:00:52 +0200

    Minor fix

--- ui.R
+++ ui.R
@@ -181,8 +181,8 @@ ui <- fluidPage(theme = bs_theme(bootswatch = "minty"),
                          h5("Navigate seamlessly in the universe of european price statistics"),
                          br(),p("This app enables to produce graphs for the all-items level or any subcomponent(s) in the european HICP (Harmonized Index of Consumer Prices). Currently graphs for indices, monthly rates, annual rates, seasonality and weights can be produced.",
                                 br(),p(" "),"For the Index tab, users can set a new reference period (month or year), common for all selected index series.",br(),p(" "),
+                                p(" "),"For the Seasonality tab, indices with December previous year = 100 are produced.",br(),p(" "),
                                 "For the M/M-12 (annual rates of change) and M/M-1 (monthly rates of change), Ribe contributions are calculated for the level directly below the selected aggregate (following formula in Eurostat's HICP Methodological manual, p 182-183). The Ribe contribution is also described in"),
-                         br(),p(" "),"For the Seasonaliy tab, indices with December previous year =100 are produced.",br(),p(" "),
                                 HTML('<a href="https://www.oecd.org/sdd/prices-ppp/OECD-calculation-contributions-annual-inflation.pdf"), target="_blank">https://www.oecd.org/sdd/prices-ppp/OECD-calculation-contributions-annual-inflation.pdf</a>'),
                          br(),p(" "), "Data is retrieved using the 'eurostat' package from the Eurostat HICP database. Additional calculations are performed where needed." 
                          
